417010413672fbd8c43be7a79eec6c441d003651
Prath <prathme47@gmail.com>
2024-06-04 23:49:43 +0530

    deleted unecessary files

--- auto booking search/booking/show_report.py
+++
@@ -1,8 +0,0 @@
-
-class BookingReport:
-    def __inti__self(self , boxex_section_element):
-        self.boxex_section_element = boxex_section_element
-        self.pull_deal_boxex = self.pull_deal_boxex()
-        
-    def pull_deal_boxex(self):
-        return self.boxex_section_element.find_elements(By.CSS_SELECTOR , 'div[data-testid="property-card"]')
